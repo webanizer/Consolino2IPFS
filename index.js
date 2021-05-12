b7e2790a26280fe97f639909f61af47d8b40cffa
Nico Krause <nico@le-space.de>
2021-05-12 06:44:43 +0100

    using imports and export statements

--- index.js
+++ index.js
@@ -1,33 +1,14 @@
 //import sha256 from "sha256"
-var sha256 = require('sha256');
-var IPFS = require('IPFS')
+import sha256 from 'sha256'
+import IPFS from 'IPFS'
+import writeToIPFS from './ipfs.js'
+import writePoEToDoichain from './doichain.js'
 
-let ipfs
+import Sleep from './consolino.js'
 
-const writeToIPFS = async (data) => {
-    const { cid } = await ipfs.add(data)
-    return cid 
-}
-
-const Sleep = (milliseconds) => {
-    return new Promise(resolve => setTimeout(resolve, milliseconds));
-}
-
-/**
- * 1. Craete Doichain transaction name_doi
- * 2. Sign transaction with PrivateKey
- * 3. Broadcast transactaction to Doichain Core
- * 
- * @param {*} cid 
- * @param {*} hash 
- */
-const writePoEToDoichain = (cid, hash) => {
-    
-
-}
 
 const main = async () => {
-    ipfs = await IPFS.create()
+    const ipfs = await IPFS.create()
 
     console.log('started reading consolino meter')
 
@@ -41,11 +22,11 @@ const main = async () => {
 
         console.log('creating sha256 hash over data')
         const hash = sha256('data');
-        console.log('our hash', hash)
-        console.log('writing data into ipfs')
-        const cid = await writeToIPFS(data)
+        console.info('our hash', hash)
+        console.info('writing data into ipfs')
+        const cid = await writeToIPFS(ipfs, data)
         
-        console.log('adding sha256 hash to Doichain')
+        console.info('adding sha256 hash and cid to Doichain')
         writePoEToDoichain(hash,cid)
     }
 }
