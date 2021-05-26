c012e4d86109beb02073662264613c6c9b7322a2
David Reband <david.reband@webanizer.de>
2021-05-26 14:21:39 +0200

    settings: getSettings, setSettings 0.0.2-5

--- index.js
+++ index.js
@@ -1,36 +1,23 @@
+import settings from "./src/Settings_.js"
 import writeData from "./src/writedata.js"
-import SmartmeterObis from 'smartmeter-obis'
+import SmartmeterObis from "smartmeter-obis"
 import IPFS from "ipfs-core"
 
-const main = async () => {  
-    
-    global.ipfs =  await IPFS.create();
+const main = async () => {
+  global.ipfs = await IPFS.create()
+  let options = settings.getSettings().options
+  
+  if (options.transportSerialPort === undefined) {
+        console.error("TransportSerialPort settings are missing")
+    return ""
+  } 
 
-    console.log('started reading consolino meter')
-
-    let options = {
-        'protocol': "SmlProtocol",
-        'transport': "SerialResponseTransport",
-        'transportSerialPort': "/dev/ttyUSB0",
-        'transportSerialBaudrate': 9600,
-        'protocolD0WakeupCharacters': 40,
-        'protocolD0DeviceAddress': '',
-        'requestInterval': 900,
-        'obisNameLanguage': 'en',
-        'obisFallbackMedium': 6,
-        'debug': 0,
-	      'protocolSmlIgnoreInvalidCRC': true,
-	      'transportSerialDataBits': 8,
-	      'transportSerialStopBits': 1,
-	      'transportSerialParity':  'none'
-      }    
-      
-    var smTransport = SmartmeterObis.init(options, writeData)
-
-    console.log('started SmartmeterObis process')  
-    smTransport.process()
-    console.log('end SmartmeterObis process') 
-    setTimeout(smTransport.stop, 120000)
+  console.log("started reading consolino meter")
+  let smTransport = SmartmeterObis.init(options, writeData)
+  console.log("started SmartmeterObis process")
+  smTransport.process()
+  console.log("end SmartmeterObis process")
+  setTimeout(smTransport.stop, 6000)
 }
 
 main()
---
+++ src/Settings-demo.js
@@ -0,0 +1,21 @@
+var SettingsTable = {
+  app: {
+    debug: "true",
+    ssl: false,
+    host: "172.20.0.9",
+    port: "3100",
+    types: ["send", "confirm", "verify"],
+  },
+  confirm: {
+    doichain: {
+      host: "localhost",
+      port: "8338",
+      username: "admin",
+      password: "......",
+      smtps: false,
+      address: "",
+    },
+  },
+};
+
+export {SettingsTable};
